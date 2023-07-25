7b9f68389cc3f2f1f13cdc567c0145c459e1c22b
y <Younes Bourakadi>
2023-07-25 14:41:22 +0200

    first commit

---
+++ database/migrations/2023_07_25_122039_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id('id_transaction');
+            $table->string('name');
+            $table->decimal('amount');
+            $table->date('date_transaction');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
